1ee1a3dacfed37c80461e044db2d86de420f3da6
liam <01liasyr@skola.boras.se>
2023-01-13 08:57:13 +0100

    js add function + btn function samt column

--- EvilScientsist.js
+++ EvilScientsist.js
@@ -1,4 +1,5 @@
-'use strict'
+'use strict';
+var _a;
 //{Name: string, Age: number, Henchmen: number} = 
 const EvilScientsitData = {
     Scinetsts: [
@@ -27,66 +28,48 @@ const EvilScientsitData = {
             Age: 24,
             Henchmen: 14,
         },
-
     ],
-    displayCurrentScinetists: function(each: any){
+    displayCurrentScinetists: function (each) {
         each.forEach(Scientstinfo => {
-
-            const Scientsitcard: any = document.querySelector('.Evil-scientist-database')
-
-            const CardForScinetist = document.createElement('section')
+            const Scientsitcard = document.querySelector('.Evil-scientist-database');
+            const CardForScinetist = document.createElement('section');
             CardForScinetist.className = 'Scientist';
-
-            Scientsitcard.append(CardForScinetist)
-
-            const h3name = document.createElement('h3')
+            Scientsitcard.append(CardForScinetist);
+            const h3name = document.createElement('h3');
             h3name.textContent = Scientstinfo.Name;
-            CardForScinetist.append(h3name)
-
-            const ScientistAge = document.createElement('h3')
+            CardForScinetist.append(h3name);
+            const ScientistAge = document.createElement('h3');
             ScientistAge.textContent = Scientstinfo.Age;
-            CardForScinetist.append(ScientistAge)
-
-            const HenchmenNumber = document.createElement('h3')
+            CardForScinetist.append(ScientistAge);
+            const HenchmenNumber = document.createElement('h3');
             HenchmenNumber.textContent = Scientstinfo.Henchmen;
-            CardForScinetist.append(HenchmenNumber)
-
+            CardForScinetist.append(HenchmenNumber);
         });
     },
     clearScientist: function () {
-        document.querySelectorAll('.Scientist').forEach(section => section.remove())
+        document.querySelectorAll('.Scientist').forEach(section => section.remove());
     },
     addScientist: function () {
-        const NameScientist: any = document.querySelector('#NamnEvilScientist')
-        
-        const AgeScientist: any = document.querySelector('#NamnEvilScientist')
-        
-        const ScientistsHenchmen: any = document.querySelector('#NamnEvilScientist')
-        
+        const NameScientist = document.querySelector('#NamnEvilScientist');
+        const AgeScientist = document.querySelector('#NamnEvilScientist');
+        const ScientistsHenchmen = document.querySelector('#NamnEvilScientist');
         const newSCientist = {
             Name: NameScientist.value,
             Age: AgeScientist.value,
             Henchmen: ScientistsHenchmen.value,
-        }
-        EvilScientsitData.Scinetsts.push(newSCientist)
-
+        };
+        EvilScientsitData.Scinetsts.push(newSCientist);
         NameScientist.value = "";
         AgeScientist.value = "";
         ScientistsHenchmen.value = "";
-
-        EvilScientsitData.displayCurrentScinetists(EvilScientsitData.Scinetsts)
+        EvilScientsitData.displayCurrentScinetists(EvilScientsitData.Scinetsts);
     },
-
-} 
-
+};
 //document.querySelector('.resetFilter-btn').addEventListener('click', function () {
 //    monsterDatabase.clearMonsters();
 //    monsterDatabase.loadMonsters(monsterDatabase.monsters);
 //});
-
-document.querySelector('.Add-sientics')?.addEventListener('click', function(){
-    EvilScientsitData.displayCurrentScinetists(EvilScientsitData.Scinetsts)
+(_a = document.querySelector('.Add-sientics')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+    EvilScientsitData.displayCurrentScinetists(EvilScientsitData.Scinetsts);
 });
-
-
-EvilScientsitData.displayCurrentScinetists(EvilScientsitData.Scinetsts)
+EvilScientsitData.displayCurrentScinetists(EvilScientsitData.Scinetsts);
